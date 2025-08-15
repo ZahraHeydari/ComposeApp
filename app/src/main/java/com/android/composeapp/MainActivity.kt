72a4dbaad6310d308e500765aa32adb9cb17e075
Zahra Heydari <heydari.z1990@gmail.com>
2025-08-15 15:02:52 +0200

    Implement a simple counter

--- app/src/main/java/com/android/composeapp/MainActivity.kt
+++ app/src/main/java/com/android/composeapp/MainActivity.kt
@@ -15,10 +15,14 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -37,48 +41,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { // Equivalent to setContentView
-            ComposeAppTheme { // For applying theme on Application
-                Greeting(name = "Android")
-            }
-        }
-    }
-}
-
-/**
- * A composable is a regular function that is annotated with the @Composable annotation
- * which produces UI elements based on the input data
- */
-@Composable
-fun Greeting(name: String) {
-    LazyColumn { // For making lazy loading lists (vertically) - items which are not on the screen are not calculated/rendered, just like RecyclerView's behavior in xml
-        items(20) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Image(
-                    painter = painterResource(R.drawable.ic_launcher_foreground),
-                    contentDescription = null,
-                    modifier = Modifier.background(Color.Blue)
-                )
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Text(
-                        text = "Hello $name!",
-                        textAlign = TextAlign.Center,
-                        color = Color.Blue,
-                        fontSize = 24.sp,
-                        // Modifier is used to apply changes to the view's appearance
-                        modifier = Modifier // Here, functions are called sequentially
-                            .padding(16.dp)
-                            .wrapContentHeight(Alignment.CenterVertically) // Centers vertically
-                    )
-                    Icon(
-                        imageVector = Icons.Default.AddCircle, contentDescription = null
-                    )
+        setContent {
+            ComposeAppTheme {
+                val count = remember {
+                    mutableIntStateOf(0) // state refers to the value that is changed over time
                 }
-                Divider(modifier = Modifier.padding(16.dp))
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = count.intValue.toString(), // Each time the value is changed, this composable is recomposed as well
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    Button(onClick = {
+                        count.intValue++
+                    }) { Text(text = "Add") }
+                }
+
             }
         }
     }
@@ -92,6 +74,6 @@ fun Greeting(name: String) {
 @Composable
 fun GreetingPreview() {
     ComposeAppTheme {
-        Greeting("Android")
+
     }
 }
